e3caaebe8bbf9888ce80fff5f8c5ec64837f2464
sarazaToussaint <tmsaraza@gmail.com>
2022-07-12 12:18:22 +0200

    Create vet_clinic database and animals table with 6 columns inside

--- schema.sql
+++ schema.sql
@@ -1,5 +1,18 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+/* Create database named vet_clinic*/
+ CREATE DATABASE vet_clinic;
+
+ /* Accessing vet_clinic database */
+  \c vet_clinic;
+ 
+ /* Create animals table with 6 columns */
+ CREATE TABLE animals(
+    id int, 
+    name varchar(20),
+    date_of_birth date,
+    escape_attemps int, 
+    neutered bool, 
+    weight_kg decimal
+    );
+
