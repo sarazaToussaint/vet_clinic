e2e3b3e087a0fb38e1eac537c1274ecec7e146e4
sarazaToussaint <tmsaraza@gmail.com>
2022-07-14 12:18:34 +0200

    Insert data in owners' table

--- data.sql
+++ data.sql
@@ -16,3 +16,12 @@ VALUES (005, 'Charmander', '2020-02-08', 0, false, -11),
        (010, 'Blossom', '1998-10-13', 3, true, 17),
 	   (011, 'Ditto', '2022-05-14', 4, true, 22);
 	   
+INSERT INTO owners (full_name,age)
+ VALUES 
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+  
