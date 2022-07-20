d8a588325f1cc159bea46c83ba23725c77f29aed
sarazaToussaint <tmsaraza@gmail.com>
2022-07-20 11:54:43 +0200

    Add indexs

--- schema.sql
+++ schema.sql
@@ -97,3 +97,10 @@ CREATE TABLE visits (
 
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+--Add index for animals_id to visits table
+CREATE INDEX animal_id_asc ON visits(animal_id ASC); 
+--Add index for vets_id to visits table
+CREATE INDEX vet_id_asc ON visits(vet_id ASC); 
+--Add index for email_id to visits table
+CREATE INDEX email_asc ON owners(email ASC); 
