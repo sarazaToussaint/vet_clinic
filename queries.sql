1ccae1eaafa398247331d3048ccd1da3ab616f50
sarazaToussaint <tmsaraza@gmail.com>
2022-07-20 10:39:55 +0200

    Add explain analize queries

--- queries.sql
+++ queries.sql
@@ -179,3 +179,13 @@ JOIN species ON species.id = animals.species_id
 WHERE vets.name = 'Maisy Smith'
 GROUP BY species.name 
 ORDER BY count DESC LIMIT 1;
+
+/*
+The following queries are taking too much time 
+(1 sec = 1000ms can be considered as too much time for database query). 
+Try them on your machine to confirm it:
+*/
+
+SELECT COUNT(*) FROM visits where animal_id = 4;
+SELECT * FROM visits where vet_id = 2;
+SELECT * FROM owners where email = 'owner_18327@mail.com';
